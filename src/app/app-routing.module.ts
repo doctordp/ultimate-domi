daaf83af40e241b0405b7537e2ace1597aa578ec
unknown <dominicdemeterca@gmail.com>
2020-10-10 18:46:28 +0200

    comment!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'info', component: InfoComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ultimate-domi';
+  toggleSidenav() {
+    document
+      .querySelector('.root-container .center .header')
+      .classList.toggle('sidenav-opened');
+    document.querySelector('.sidenav').classList.toggle('opened');
+    document
+      .querySelector('.root-container__full-overlay')
+      .classList.toggle('opened');
+    document
+      .querySelector('.root-container .center .header .title-button')
+      .classList.toggle('close');
+  }
 }

ec935219c7f51e2d49d8c1ec9373322c6549c4b7
unknown <dominicdemeterca@gmail.com>
2020-10-11 17:58:02 +0200

    litle changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,10 @@ export class AppComponent {
       .querySelector('.root-container__full-overlay')
       .classList.toggle('opened');
     document
-      .querySelector('.root-container .center .header .title-button')
+      .querySelector('.root-container .center .header .openSidenavButton')
+      .classList.toggle('close');
+    document
+      .querySelector('.root-container .sidenav .closebtn')
       .classList.toggle('close');
   }
 }
